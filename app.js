15e373f36a27a7e39bff7f81baa816c2423ef1e8
Rahul Dange <rahuldangeofficial@gmail.com>
2023-08-09 16:08:44 +0530

    Refactor code for improved modularity and readability

--- app.js
+++ app.js
@@ -3,44 +3,37 @@ const fs = require("fs");
 
 const server = http.createServer((req, res) => {
   const url = req.url;
-  if (url === "/") {
-    fs.readFile("./pages/index.html", "utf8", (err, content) => {
-      if (err) {
-        throw err;
-      } else {
-        res.writeHead(200, { "Content-Type": "text/html" });
-        res.end(content);
-      }
-    });
-  } else if (url === "/about") {
-    fs.readFile("./pages/about.html", "utf8", (err, content) => {
-      if (err) {
-        throw err;
-      } else {
-        res.writeHead(200, { "Content-Type": "text/html" });
-        res.end(content);
-      }
-    });
-  } else if (url === "/contact") {
-    fs.readFile(".pages/contact.html", "utf8", (err, content) => {
-      if (err) {
-        throw err;
-      } else {
-        res.writeHead(200, { "Content-Type": "text/html" });
-        res.end(content);
-      }
-    });
-  } else {
-    fs.readFile("./pages/404.html", "utf8", (err, content) => {
-      if (err) {
-        throw err;
-      } else {
-        res.writeHead(200, { "Content-Type": "text/html" });
-        res.end(content);
-      }
-    });
-  }
+  servePage(url, res);
 });
+
+function servePage(url, res) {
+  let fileName = "";
+
+  switch (url) {
+    case "/":
+      fileName = "index.html";
+      break;
+    case "/about":
+      fileName = "about.html";
+      break;
+    case "/contact":
+      fileName = "contact.html";
+      break;
+    default:
+      fileName = "404.html";
+      break;
+  }
+
+  fs.readFile(fileName, "utf8", (err, content) => {
+    if (err) {
+      res.writeHead(500, { "Content-Type": "text/plain" });
+      res.end("Internal Server Error");
+    } else {
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.end(content);
+    }
+  });
+}
 
 server.listen(8080, () => {
   console.log("Server is running on port 8080");
